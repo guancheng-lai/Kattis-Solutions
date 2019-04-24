b5c8854e8cfcc93497a13ab232d7e9cf36385a9b
guancheng lai <glai@harding.edu>
2019-04-23 23:36:31 -0500

    Initial Commit

---
+++ Kattis - Rounded Buttons/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kattis - Rounded Buttons
+//
+//  Created by Guancheng Lai on 4/23/19.
+//  Copyright © 2019 Guancheng Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
