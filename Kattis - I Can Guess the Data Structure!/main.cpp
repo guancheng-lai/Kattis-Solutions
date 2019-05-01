a93708d15a0e326b70adf514edbce04fa3176365
guancheng lai <glai@harding.edu>
2019-04-30 19:37:52 -0500

    Initial Commit

---
+++ Kattis - I Can Guess the Data Structure!/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kattis - I Can Guess the Data Structure!
+//
+//  Created by Guancheng Lai on 4/30/19.
+//  Copyright © 2019 Guancheng Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
