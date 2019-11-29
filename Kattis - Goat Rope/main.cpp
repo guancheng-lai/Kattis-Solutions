b5e8438fe4f43524b247c4385e6d9f573c16e394
Guancheng Lai <lgc20407@gmail.com>
2019-11-29 01:56:46 -0500

    change

--- Kattis - Goat Rope/main.cpp
+++ Kattis - Goat Rope/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(int argc, const char * argv[])
+{
+    int x, y, x1, y1, x2, y2;
+    cin >> x >> y >> x1 >> y1 >> x2 >> y2;
+    
+    if (x < x1 && y < y1)
+    {
+        cout << std::fixed << sqrt((x - x1)*(x - x1) + (y - y1) * (y - y1)) << endl;
+    }
+    else if (x < x1 && (y1 <= y && y <= y2))
+    {
+        cout << std::fixed << double (x1 - x) << endl;
+    }
+    else if (x < x1 && y2 < y)
+    {
+        cout << std::fixed << sqrt((x - x1) * (x - x1) + (y - y2) * (y - y2)) << endl;
+    }
+    else if ((x1 <= x && x <= x2) && y < y1)
+    {
+        cout << std::fixed << double (y1 - y) << endl;
+    }
+    else if ((x1 <= x && x <= x2) && y2 < y)
+    {
+        cout << std::fixed << double (y - y2) << endl;
+    }
+    else if (x2 < x && y < y1)
+    {
+        cout << std::fixed << sqrt((x - x2) * (x - x2) + (y - y1) * (y - y1)) << endl;
+    }
+    else if (x2 < x && (y1 <= y && y <= y2))
+    {
+        cout << std::fixed << double (x - x2) << endl;
+    }
+    else if (x2 < x && y2 < y)
+    {
+        cout << std::fixed << sqrt((x - x2) * (x - x2) + (y - y2) * (y - y2)) << endl;
+    }
 }
